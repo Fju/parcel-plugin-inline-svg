d3d66c7850e5d19efa72babcafa595404945a13b
Fju <flowi@outlook.com>
2020-05-15 19:23:39 +0200

    Added support for config files

--- asset.js
+++ asset.js
@@ -1,18 +1,30 @@
 const { Asset } = require('parcel-bundler');
 const svgo = require("svgo");
 
-const optimizer = new svgo({
-	plugins: [{
-		removeDoctype: true
-	},
-	{
-		removeComments: true
-	},
-	{
-		removeXMLNS: true
+const fs = require('fs');
+const path = require('path');
+
+function loadUserConfig() {
+    var file;
+	
+	// try loading .config.js file	
+	file = path.resolve(process.cwd(), 'inline-svg.config.js');
+	if (fs.existsSync(file)) {
+		return require(file);	
 	}
-	]
-});
+
+	// try loading .config.json file
+	file = path.resolve(process.cwd(), 'inline-svg.config.json');
+	if (fs.existsSync(file)) {
+		return JSON.parse(fs.readFileSync(file, { encoding: 'utf-8' }));
+	}
+
+    return {};
+}
+
+var config = loadUserConfig();
+
+const optimizer = new svgo(config.svgo || {});
 
 class InlineSVGAsset extends Asset {
 	constructor(name, options) {
